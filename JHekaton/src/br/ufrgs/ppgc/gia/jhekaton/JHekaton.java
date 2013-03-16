1da8a5e45a854e23306696e0130dba1c8c7803f8
igormontezano <igor.montezano@gmail.com>
2013-03-16 12:59:55 -0300

    Comit inicial do projeto

---
+++ JHekaton/src/br/ufrgs/ppgc/gia/jhekaton/JHekaton.java
@@ -0,0 +1,14 @@
+package br.ufrgs.ppgc.gia.jhekaton;
+
+/**
+ * Classe que contém o método de inicialização da aplicação
+ * @author Igor Montezano - imsantos@inf.ufrgs.br
+ *
+ */
+public class JHekaton {
+
+	public static void main(String[] args) {
+
+	}
+
+}
