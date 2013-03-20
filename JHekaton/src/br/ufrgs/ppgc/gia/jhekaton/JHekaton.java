fa7b039419a3771fff325bf395428edf000bd098
igormontezano <igor.montezano@gmail.com>
2013-03-20 07:52:13 -0300

    utilização da lib de NN e código exemplo

--- JHekaton/src/br/ufrgs/ppgc/gia/jhekaton/JHekaton.java
+++ JHekaton/src/br/ufrgs/ppgc/gia/jhekaton/JHekaton.java
@@ -1,5 +1,23 @@
 package br.ufrgs.ppgc.gia.jhekaton;
 
+import java.io.File;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import org.encog.Encog;
+import org.encog.engine.network.activation.ActivationSigmoid;
+import org.encog.ml.data.MLData;
+import org.encog.ml.data.MLDataPair;
+import org.encog.ml.data.MLDataSet;
+import org.encog.ml.data.basic.BasicMLDataSet;
+import org.encog.neural.networks.BasicNetwork;
+import org.encog.neural.networks.layers.BasicLayer;
+import org.encog.neural.networks.training.propagation.resilient.ResilientPropagation;
+
+import br.ufrgs.ppgc.gia.jhekaton.xml.Model;
+
 /**
  * Classe que contém o método de inicialização da aplicação
  * @author Igor Montezano - imsantos@inf.ufrgs.br
@@ -8,7 +26,76 @@ package br.ufrgs.ppgc.gia.jhekaton;
 public class JHekaton {
 
 	public static void main(String[] args) {
-
+		
+		File file = new File("src/model.uml");
+		JAXBContext jaxbContext;
+		try {
+			jaxbContext = JAXBContext.newInstance(Model.class);
+			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+			Model customer = (Model) jaxbUnmarshaller.unmarshal(file);
+			System.out.println(customer);
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		
 	}
-
+	
 }
+
+/* Exemplo de NN */
+class XORHelloWorld {
+
+	/**
+	 * The input necessary for XOR.
+	 */
+	public static double XOR_INPUT[][] = { { 0.0, 0.0 }, { 1.0, 0.0 },
+			{ 0.0, 1.0 }, { 1.0, 1.0 } };
+
+	/**
+	 * The ideal data necessary for XOR.
+	 */
+	public static double XOR_IDEAL[][] = { { 0.0 }, { 1.0 }, { 1.0 }, { 0.0 } };
+	
+	/**
+	 * The main method.
+	 * @param args No arguments are used.
+	 */
+	public static void main(final String args[]) {
+		
+		// create a neural network, without using a factory
+		BasicNetwork network = new BasicNetwork();
+		network.addLayer(new BasicLayer(null,true,2));
+		network.addLayer(new BasicLayer(new ActivationSigmoid(),true,3));
+		network.addLayer(new BasicLayer(new ActivationSigmoid(),false,1));
+		network.getStructure().finalizeStructure();
+		network.reset();
+
+		// create training data
+		MLDataSet trainingSet = new BasicMLDataSet(XOR_INPUT, XOR_IDEAL);
+		
+		// train the neural network
+		final ResilientPropagation train = new ResilientPropagation(network, trainingSet);
+
+		int epoch = 1;
+
+		do {
+			train.iteration();
+			System.out.println("Epoch #" + epoch + " Error:" + train.getError());
+			epoch++;
+		} while(train.getError() > 0.01);
+
+		// test the neural network
+		System.out.println("Neural Network Results:");
+		for(MLDataPair pair: trainingSet ) {
+			final MLData output = network.compute(pair.getInput());
+			System.out.println(pair.getInput().getData(0) + "," + pair.getInput().getData(1)
+					+ ", actual=" + output.getData(0) + ",ideal=" + pair.getIdeal().getData(0));
+		}
+		
+		Encog.getInstance().shutdown();
+	}
+}
+
