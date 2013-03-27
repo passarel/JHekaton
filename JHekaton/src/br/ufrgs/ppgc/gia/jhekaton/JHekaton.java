f75d3960766ae3e41ab9837cf659b581be0b7b21
igormontezano <igor.montezano@gmail.com>
2013-03-26 23:17:28 -0300

    Feito treinamento

--- JHekaton/src/br/ufrgs/ppgc/gia/jhekaton/JHekaton.java
+++ JHekaton/src/br/ufrgs/ppgc/gia/jhekaton/JHekaton.java
@@ -6,28 +6,24 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 
-import org.encog.Encog;
+import org.encog.engine.network.activation.ActivationCompetitive;
+import org.encog.engine.network.activation.ActivationElliott;
 import org.encog.engine.network.activation.ActivationSigmoid;
-import org.encog.ml.MLMethod;
+import org.encog.engine.network.activation.ActivationStep;
 import org.encog.ml.data.MLData;
-import org.encog.ml.data.MLDataPair;
 import org.encog.ml.data.MLDataSet;
+import org.encog.ml.data.basic.BasicMLData;
 import org.encog.ml.data.basic.BasicMLDataSet;
-import org.encog.ml.data.specific.BiPolarNeuralData;
-import org.encog.ml.data.temporal.TemporalMLDataSet;
 import org.encog.ml.train.MLTrain;
 import org.encog.ml.train.strategy.Greedy;
 import org.encog.ml.train.strategy.HybridStrategy;
 import org.encog.ml.train.strategy.StopTrainingStrategy;
-import org.encog.neural.bam.BAM;
 import org.encog.neural.networks.BasicNetwork;
-import org.encog.neural.networks.NeuralDataMapping;
-import org.encog.neural.networks.layers.BasicLayer;
 import org.encog.neural.networks.training.CalculateScore;
 import org.encog.neural.networks.training.TrainingSetScore;
 import org.encog.neural.networks.training.anneal.NeuralSimulatedAnnealing;
 import org.encog.neural.networks.training.propagation.back.Backpropagation;
-import org.encog.neural.networks.training.propagation.resilient.ResilientPropagation;
+import org.encog.neural.pattern.ElmanPattern;
 import org.encog.neural.pattern.JordanPattern;
 
 import br.ufrgs.ppgc.gia.jhekaton.xml.Model;
@@ -41,122 +37,146 @@ import br.ufrgs.ppgc.gia.jhekaton.xml.Model.PackagedElement.Region.Transition;
  */
 public class JHekaton {
 
+	// Matriz de entradas
+	private static final double[][] ENTRADAS = new double[][]{
+//		{Ir,C1,C2,F1,F2,II,S0,S1,S2}, Rótulos 
+		{1d,0d,0d,0d,0d,1d,0d,0d,0d}, // Instancia 1
+		{0d,1d,0d,0d,0d,0d,1d,0d,0d}, // Instancia 2
+		{0d,0d,1d,0d,0d,0d,1d,0d,0d}, // Instancia 3
+		{0d,0d,0d,1d,0d,0d,0d,1d,0d}, // Instancia 4
+		{0d,0d,0d,0d,1d,0d,0d,0d,1d}  // Instancia 5
+	};
+				
+				// Matriz de saídas
+	private static final double[][] SAIDAS = new double[][]{
+//		{S0,S1,S2,FF}, Rótulos
+		{1d,0d,0d,0d}, // Instancia 1
+		{0d,1d,0d,0d}, // Instancia 2
+		{0d,0d,1d,0d}, // Instancia 3
+		{0d,0d,0d,1d}, // Instancia 4
+		{0d,0d,0d,1d}  // Instancia 5
+	};
+	
+	private static final int TESTE = 1;
+	
 	public static void main(String[] args) {
 		
+		// Arquivo Origem
 		File file = new File("src/model.uml");
+		
 		JAXBContext jaxbContext;
 		try {
+			// Instancia o Parser
 			jaxbContext = JAXBContext.newInstance(Model.class);
 			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+			
+			//Parseia a entrada
 			Model customer = (Model) jaxbUnmarshaller.unmarshal(file);
 			
-			
 			int countEntradas = 0, countSaidas = 0;
+			
+			// Itera sobre os estados
 			for(Subvertex sub : customer.getPackagedElement().getRegion().getSubvertex()){
 				System.out.println("Estado: "+sub.getName()+"-"+sub.getType());
+				
+				// Conta os estados como entradas, descontando o "estado final"
 				if(!sub.getType().equalsIgnoreCase("uml:FinalState")){
 					countEntradas++;
 				}
+				
+				// Conta os estados como saída, descontando o "estado inicial"
 				if(!sub.getType().equalsIgnoreCase("uml:Pseudostate")){
 					countSaidas++;
 				}
 			}
+			
+			// Itera sobre as transições
 			for(Transition t : customer.getPackagedElement().getRegion().getTransition()){
 				System.out.println("Transicao: "+t.getName());
 				countEntradas++;
 			}
 			System.out.println(countEntradas + ":"+countSaidas);
+
+			
+//			ElmanPattern pattern = new ElmanPattern();
+//			pattern.setActivationFunction(new ActivationSigmoid());
+//			pattern.setInputNeurons(countEntradas);
+//			pattern.addHiddenLayer(5);
+//			pattern.setOutputNeurons(countSaidas);
+//			BasicNetwork rede = (BasicNetwork)pattern.generate();
+			
 			
 			// Criação da rede, com 2 camadas
 			JordanPattern pattern = new JordanPattern();
 			pattern.setActivationFunction(new ActivationSigmoid());
+			// Cria rede com camada de entrada dimensionada pela contagem 
 			pattern.setInputNeurons(countEntradas);
-			pattern.addHiddenLayer(1);
+
+			// configura a rede com apenas uma camada oculta
+			pattern.addHiddenLayer(5);
+			
+			// ... camada de saída dimensionada pela contagem
 			pattern.setOutputNeurons(countSaidas);
 			BasicNetwork rede = (BasicNetwork) pattern.generate();
 			
-			// train the neural network
-			BiPolarNeuralData data1 = new BiPolarNeuralData(new boolean[]{true,false,false,false,false,true,false,false,false,false});
-			BiPolarNeuralData data2 = new BiPolarNeuralData(new boolean[]{true,false,false,false});
-			
-			MLDataSet trainingSet;
+			MLDataSet trainingSet = new BasicMLDataSet(ENTRADAS,SAIDAS) ;
 			
 			CalculateScore score = new TrainingSetScore(trainingSet);
-			final MLTrain trainAlt = new NeuralSimulatedAnnealing(rede, score, 10, 2, 100);
 
-			final MLTrain trainMain = new Backpropagation(rede, trainingSet,0.00001, 0.0);
+			final MLTrain trainMain = new Backpropagation(rede, trainingSet, 0.0000000001, 0.01);
+			final MLTrain trainAlt = new NeuralSimulatedAnnealing(rede, score, 10, 2, 100);
 
 			final StopTrainingStrategy stop = new StopTrainingStrategy();
 			trainMain.addStrategy(new Greedy());
 			trainMain.addStrategy(new HybridStrategy(trainAlt));
-			//trainMain.addStrategy(stop);
+			trainMain.addStrategy(stop);
 
 			int epoch = 0;
 			while (!stop.shouldStop()) {
+				// cada iteração é uma época
 				trainMain.iteration();
 				System.out.println("Epoch #" + epoch	+ " Error:" + trainMain.getError());
 				epoch++;
 			}
-			trainMain.getError();
+			trainMain.finishTraining();
+			System.out.println("Erro Final apos treinamento: "+trainMain.getError());
+			
+			// teste 
+			System.out.println("Caso de teste:");
+			for(double i : ENTRADAS[TESTE]){
+				System.out.print(i+"\t");
+			}
+			System.out.println();
+			System.out.println("Resultado esperado:");
+			
+			for(double i : SAIDAS[TESTE]){
+				System.out.print(i+"\t");
+			}
+
+			System.out.println();
+			double[] teste1 = ENTRADAS[TESTE]; // Instancia 1;
+			MLData testeSet = new BasicMLData(teste1) ;
+			MLData result = rede.compute(testeSet);
+			System.out.println("Resultado arredondado:");
+			double maior = 0d;
+			int index = -1;
+			for(int i = 0 ; i < result.getData().length; i++){
+				if(maior < result.getData()[i]){
+					maior = result.getData()[i];
+					index = i;
+				}
+				System.out.print(Math.round(result.getData()[i])+"\t");
+			}
+			System.out.println();
+			System.out.println("Final: "+index);
+			System.out.println("Resultado real:");
+			for(double i : result.getData()){
+				System.out.print(i+"\t");
+			}
+
 			
 		} catch (JAXBException e) {
 			e.printStackTrace();
 		}
-	}
-	
+	}	
 }
-
-/* Exemplo de NN */
-class XORHelloWorld {
-
-	/**
-	 * The input necessary for XOR.
-	 */
-	public static double XOR_INPUT[][] = { { 0.0, 0.0 }, { 1.0, 0.0 },
-			{ 0.0, 1.0 }, { 1.0, 1.0 } };
-
-	/**
-	 * The ideal data necessary for XOR.
-	 */
-	public static double XOR_IDEAL[][] = { { 0.0 }, { 1.0 }, { 1.0 }, { 0.0 } };
-	
-	/**
-	 * The main method.
-	 * @param args No arguments are used.
-	 */
-	public static void main(final String args[]) {
-		
-		// create a neural network, without using a factory
-		BasicNetwork network = new BasicNetwork();
-		network.addLayer(new BasicLayer(null,true,2));
-		network.addLayer(new BasicLayer(new ActivationSigmoid(),true,3));
-		network.addLayer(new BasicLayer(new ActivationSigmoid(),false,1));
-		network.getStructure().finalizeStructure();
-		network.reset();
-
-		// create training data
-		MLDataSet trainingSet = new BasicMLDataSet(XOR_INPUT, XOR_IDEAL);
-		
-		// train the neural network
-		final ResilientPropagation train = new ResilientPropagation(network, trainingSet);
-
-		int epoch = 1;
-
-		do {
-			train.iteration();
-			System.out.println("Epoch #" + epoch + " Error:" + train.getError());
-			epoch++;
-		} while(train.getError() > 0.01);
-
-		// test the neural network
-		System.out.println("Neural Network Results:");
-		for(MLDataPair pair: trainingSet ) {
-			final MLData output = network.compute(pair.getInput());
-			System.out.println(pair.getInput().getData(0) + "," + pair.getInput().getData(1)
-					+ ", actual=" + output.getData(0) + ",ideal=" + pair.getIdeal().getData(0));
-		}
-		
-		Encog.getInstance().shutdown();
-	}
-}
-
